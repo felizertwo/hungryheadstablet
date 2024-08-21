bc04855b8b7092b0fa3c9f76afe87e3640b254c9
felizertwo <f.eigenbrodt@sb-konzept.de>
2024-08-21 17:44:57 +0200

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,17 @@
-"use client"
+"use client";
 
 import { Inter } from "next/font/google";
 import "./globals.css";
-import {ApiProvider} from "@/app/context/ApiContext";
+import { ApiProvider } from "@/app/context/ApiContext";
 import ProtectedRoute from "@/app/components/ProtectedRoute";
 import React from "react";
-import {CartProvider} from "@/app/context/CartContext";
+import { CartProvider } from "@/app/context/CartContext";
 import { usePathname } from "next/navigation";
 import styled from "styled-components";
 import InactivityRedirect from "@/app/components/InactivityRedirect";
 import TabletErrorBoundary from "@/app/components/ErrorBoundary";
 
 const inter = Inter({ subsets: ["latin"] });
-
 
 const Wrapper = styled.div`
   display: flex;
@@ -23,25 +22,26 @@ const Wrapper = styled.div`
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
-  const noAuthRequired = ['/login'];
+  const noAuthRequired = ["/login"];
 
   return (
     <html lang="en">
       <body className={inter.className}>
-          <TabletErrorBoundary>
-              <ApiProvider>
-                {noAuthRequired.includes(pathname) ? (
-                    <Wrapper>{children}</Wrapper>
-                ) : (
-                    <ProtectedRoute>
-                        <InactivityRedirect timeout={120_000} warningTime={10_000} />
-                        <CartProvider>
-                          <Wrapper> {children}</Wrapper>
-                        </CartProvider>
-                    </ProtectedRoute>
-                )}
-              </ApiProvider>
-          </TabletErrorBoundary>
+        <TabletErrorBoundary>
+          <ApiProvider>
+            {noAuthRequired.includes(pathname) ? (
+              <Wrapper>{children}</Wrapper>
+            ) : (
+              <ProtectedRoute>
+                <InactivityRedirect timeout={120_000} warningTime={10_000} />
+                <CartProvider>
+                  <Wrapper> {children}</Wrapper>
+                </CartProvider>
+              </ProtectedRoute>
+            )}
+          </ApiProvider>
+        </TabletErrorBoundary>
+        <script src={cordovaUrl} />
       </body>
     </html>
   );
